9e486c92d2b2f0065edd8741123e943ba3b00420
BrunoTouron <bruno_touron@hotmail.com>
2016-09-24 12:33:25 -0300

    Add files via upload

---
+++ UserInfluentes/Controllers/UsersApiController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.Web.Http;
+using System.Net.Http;
+using System.Web.Mvc;
+using UserInfluentes.data;
+using UserInfluentes.Bus;
+
+namespace UserInfluentes.Controllers
+{
+    public class UsersApiController : ApiController
+    {
+        public IEnumerable<Brands> GetInteractions()
+        {
+            var usersBO = new UsersBO();
+
+            usersBO.GetBrands();
+            usersBO.GetUsers();
+            usersBO.GetInteractions();
+
+            return usersBO.GetBrands();
+        }
+    }
+}
