64733c9014f6d3c34f64f40524f2978b11ed9681
BrunoTouron <bruno_touron@hotmail.com>
2016-09-25 17:24:14 -0300

    Add files via upload

---
+++ UserInfluentes.data/_Interactions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserInfluentes.data
+{
+    public class _Interactions
+    {
+        public _Interactions()
+        {
+        }
+        public _Interactions(int brand, int user, string type, string text)
+        {
+
+            _brand = brand;
+            _user = user;
+            _type = type;
+            _text = text;
+        }
+
+        private int _brand;
+
+        public int Brand
+        {
+            get { return _brand; }
+            set { _brand = value; }
+        }
+
+        private int _user;
+
+        public int User
+        {
+            get { return _user; }
+            set { _user = value; }
+        }
+       
+        private string _type;
+        public string Type
+        {
+            get { return _type; }
+            set { _type = value; }
+        }
+
+        private string _text;
+        public string Text
+        {
+            get { return _text; }
+            set { _text = value; }
+        }
+    }
+}
---
+++ UserInfluentes.data/__Interactions.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserInfluentes.data
+{
+    public class __Interactions
+    {
+        public __Interactions()
+        {
+        }
+        public __Interactions(int brand, string brandDesc, int user, string userDesc, int quantidade)
+        {
+            _brand = brand;
+            _brandDesc = brandDesc;
+            _user = user;
+            _userDesc = userDesc;
+            _quantidade = quantidade;
+        }
+
+        private int _brand;
+
+        public int Brand
+        {
+            get { return _brand; }
+            set { _brand = value; }
+        }
+
+        private string _brandDesc;
+
+        public string BrandDesc
+        {
+            get { return _brandDesc; }
+            set { _brandDesc = value; }
+        }
+
+        private int _user;
+
+        public int User
+        {
+            get { return _user; }
+            set { _user = value; }
+        }
+        private string _userDesc;
+
+        public string UserDesc
+        {
+            get { return _userDesc; }
+            set { _userDesc = value; }
+        }
+
+        private int _quantidade;
+
+        public int Quantidade
+        {
+            get { return _quantidade; }
+            set { _quantidade = value; }
+        }
+    }
+}
